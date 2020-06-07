797870aa54a54c22a3bfc53d1f08bd4a0600abe1
keike <keisuke.yamada@keik.co.jp>
2020-06-07 17:52:58 +0900

    Analytics他の方法試し

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,35 +1,19 @@
-import Document, { Head, Main, NextScript } from 'next/document';
-
-import { GA_TRACKING_ID } from '../lib/gtag'
-
-export default class extends Document {
+export default class MyDocument extends Document {
   render() {
+    const GA_TRACKING_ID = 'UA-168771799-1'
+    const gtmScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GA_TRACKING_ID}');`
+    const gtmFrame = `<iframe src="https://www.googletagmanager.com/ns.html?id=${GA_TRACKING_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
     return (
-      <html>
+      <Html lang="en">
         <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
+          <script dangerouslySetInnerHTML={{ __html: gtmScript }} />
         </Head>
         <body>
+          <noscript dangerouslySetInnerHTML={{ __html: gtmFrame }} />
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     )
   }
 }
