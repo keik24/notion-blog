652b5e320fe7d1325bca648b13482c8367bdc8bd
keike <keisuke.yamada@keik.co.jp>
2020-06-07 17:51:14 +0900

    Revert "Analyticsベタ貼りにしてみる"
    This reverts commit e74964b90930cb50e004ded20cd86decc7f9f27d.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,19 +7,23 @@ export default class extends Document {
     return (
       <html>
         <Head>
-    <script
-      async
-      src={`https://www.googletagmanager.com/gtag/js?id=UA-168771799-1`}
-    />
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'UA-168771799-1');
-            `,
-      }}
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
